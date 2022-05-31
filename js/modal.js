e184d63b09d184a6cf3e77daf18f0592e9da6c3b
Denys <zpdeni10@gmail.com>
2022-05-31 20:08:24 +0300

    Правки 9
    Модальные окна открываются на каждой кнопке.
    Добавлена кнопка "Обратный звонок"
    Подключены Js скрипты, но пока не работают

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const modal = document.getElementById('myModal');
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+const myBtn = document.getElementsByClassName('myBtn');
+const close = document.getElementsByClassName('close')[0];
+/*
+А это в цикле прокруциваем те элементы которыми мы открываем модал окно
+и обработчик события который открывет окно
+*/
+for (let i = 0; i < myBtn.length; i++) {
+  myBtn[i].addEventListener('click', function() {
+    openModalWindow();
+  })
+}
+// это обработчик события, который в нутри этой функции выполнят ту функцию 
+//которая закрывает окно модальное
+close.addEventListener('click', function() {
+  closeModalWindow();
+})
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//это функция, которая открывает окно
+function openModalWindow() {
+  modal.style.display = "block";
+}
+
+//это функция, которая закрывает окно
+function closeModalWindow() {
+  modal.style.display = "none";
+}
